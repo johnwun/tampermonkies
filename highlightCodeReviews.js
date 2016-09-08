3da78ed265e14c8317cef73aa9ee4882db51ea51
John Wundes <johnwun@gmail.com>
2016-09-08 10:53:50 -0700

    Adding Highlight Reviews
    adds a little color to the review lines. Allows user to quickly see if a ticket has been reviewed, and how many times.
    Adds custom colors for:
     - Code Review +1 
     - Code Review -1
     - Code Review +2
     - Code Review -2
     - Code Review 0
     - Merged Status

---
+++ highlightCodeReviews.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         Highlight Reviews
+// @namespace    http://wundes.com/
+// @version      0.0.1
+// @description  highlights code reviews as red and green, or blue for comments only
+// @author       John Wundes
+// @include https://gerrit.nexgen.neustar.biz/*
+// @grant        none
+// ==/UserScript==
+
+var $$ = document.querySelector.bind(document);
+var $$all = document.querySelectorAll.bind(document);
+var pollFrequency = 1000; //milliseconds
+var timer2;
+var checkChanges = function() {
+  [].filter.call($$all('.com-google-gerrit-client-change-Message_BinderImpl_GenCss_style-closed'), (e) => {
+      return e.textContent.match('Code-Review') ||
+             e.textContent.match('successfully merged');
+  }).filter((e) => {
+      return typeof e === 'object';}).map((e) => {
+        if(e.textContent.match('Review[+]1')){
+            // light green
+            e.style.backgroundColor = '#dfd';}
+        else if(e.textContent.match('Review[+]2')){
+            // dark green
+            e.style.backgroundColor = '#9f9';}
+        else if (e.textContent.match('Review[-]1')) {
+            // light red
+            e.style.backgroundColor = '#fdd';
+        }
+        else if (e.textContent.match('Review[-]2')) {
+            // dark red with red text
+            e.style.backgroundColor = '#f99';
+            [].map.call(e.getElementsByTagName("div"),(function(c){c.style.color = '#c00';}));
+        } else if (e.textContent.match('merged')) {
+            // black with green text
+            e.style.backgroundColor = '#666';
+            [].map.call(e.getElementsByTagName("div"),(function(c){c.style.color = '#dfd';}));
+        } else if (e.textContent.match('-Code-Review')) {
+            // info
+            e.style.backgroundColor = '#eeecff';
+        }
+  });
+  highlightPoller();
+};
+
+// HACK: don't know how to make this fire on url update for spa,
+// so just polling every second to update button status.
+var highlightPoller = function() {
+  if (document.getElementsByClassName('topmenuMenuLeft').length) {
+    //update every x milliseconds if we change location.
+    timer2 = setTimeout(checkChanges, pollFrequency);
+  } else {
+    timer2 = setTimeout(highlightPoller, pollFrequency);
+  }
+};
+
+highlightPoller();
