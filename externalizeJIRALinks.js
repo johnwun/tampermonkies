e05746130578d6a3a84135cda68f16e30c1c7ace
John Wundes <johnwun@gmail.com>
2017-05-04 10:25:37 -0700

    Create externalizeJIRALinks.js

---
+++ externalizeJIRALinks.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Make all board links external
+// @namespace    http://wundes.com/
+// @version      0.0.1
+// @description  makes all openable ticket links appear green, and open in new tab.
+// @author       John Wundes
+// @match        https://jira.nexgen.neustar.biz/secure/RapidBoard.jspa*
+// @grant        none
+// ==/UserScript==
+var $$ = document.querySelector.bind(document);
+var $$all = document.querySelectorAll.bind(document);
+var pollFrequency = 1000; //milliseconds
+var timer2;
+var checkChanges = function() {
+    console.log('tick');
+  [].filter.call($$all('a'), (e) => {
+      return e.href && e.href.match(/browse/);
+  }).filter((e) => {
+      return typeof e === 'object';}).map((e) => {
+        if(1){
+            // medium green
+            e.style.color = '#090';
+            e.target='_blank';
+        }
+  });
+  highlightPoller();
+};
+
+// HACK: don't know how to make this fire on url update for spa,
+// so just polling every second to update button status.
+var highlightPoller = function() {
+  if (true) {
+    //update every x milliseconds if we change location.
+    timer2 = setTimeout(checkChanges, pollFrequency);
+  } else {
+    timer2 = setTimeout(highlightPoller, pollFrequency);
+  }
+};
+
+highlightPoller();
